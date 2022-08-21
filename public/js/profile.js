d6704aa384d104290df312888defde7c901cfb86
Lizzle <leefboettcher@gmail.com>
2022-08-21 00:22:08 -0500

    post working

--- public/js/profile.js
+++ public/js/profile.js
@@ -25,7 +25,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/post/${id}`, {
+    const response = await fetch(`/post/${id}`, {
       method: 'DELETE',
     });
 
